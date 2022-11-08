0a5f8808fd15147e00203a54f6289b11266e1a48
Sol1kk <113501863+Sol1kk@users.noreply.github.com>
2022-11-08 14:17:25 +0300

    Add files via upload

---
+++ zxc.pas
@@ -0,0 +1,15 @@
+﻿program kr1;
+ var x,y,h,c,a:real;
+ begin
+   writeln('Введите промежуток a and c and h');
+   read(a,c,h);
+   x:=a;
+   while x<=c do 
+   begin
+     if (x<-9) then y:=-x*exp(x)-x
+     else if (x<1) then y:=cos(x) * log(x) + 82/x
+     else y:=(x*cos(x) - tan(x));
+     writeln('x=',x:1:2, ' y=',y:1:2);
+     x+=h;
+   end;
+ end.
